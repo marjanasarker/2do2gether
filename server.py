147daf18ac2c2eed5982cc8f125909fb70a28e87
Marjana Sarker <msarker86@gmail.com>
2021-04-07 02:26:55 +0000

    Barebones server.py with running flask application

--- server.py
+++ server.py
@@ -1,1 +1,14 @@
 from flask import Flask, render_template
+from model import connect_to_db
+
+app = Flask(__name__)
+app.secret_key = "dev"
+#app.jinja_env.undefined = StrictUndefined
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    connect_to_db(app)
+    app.run(host='0.0.0.0', debug=True)
