fd6eae1507f3dada4bffa9c177c6aa4cab278337
Marjana Sarker <msarker86@gmail.com>
2021-04-08 16:05:30 +0000

    Imported additional methods from flask, request, flash, session, redirect to run functions, added a create account function for new users and a regular login function for current users

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, flash, session, redirect, render_template
 from model import connect_to_db
 import crud
 from jinja2 import StrictUndefined
@@ -12,14 +12,14 @@ def homepage():
     
     return render_template('homepage.html')
 
-@app.route('/login')
-def show_login_form(): #get the login page
+@app.route('/create_account')
+def show_newaccount_form(): 
 
 
-    return render_template('login.html')
+    return render_template('create_account.html')
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/create_account', methods=['POST'])
 def register_user():
     """Creating new user"""
 
@@ -29,18 +29,27 @@ def register_user():
     password = request.form.get('password')
 
     user = crud.get_user_by_email(email)
+    
 
     if user:
-        flash('Cannot create an account with that email. Try again.')
+        flash('Cannot create an account, email on file. Try again.')
     else:
-        crud.create_user(fname, lname, email, password)
-        flash('Account created! Please log in.')
+        new_user = crud.create_user(first_name, last_name, email, password)
+        flash('Account created! Please log in.') #where should I redirect them 
 
     return redirect('/login') #redirect to a profile page 
 
 
     #return render_template('login.html')
+@app.route('/login')
+def regular_user_login():
 
+    return render_template('login.html')
+
+#@app.route('/login', methods=['POST'] )
+#def regular_user_login():
+
+    #redirect('login.html')  change this
     
 
 if __name__ == '__main__':
