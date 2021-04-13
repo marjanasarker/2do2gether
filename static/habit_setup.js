37e8c4ca2e99041c7c20a48b0267ede9b9146617
Marjana Sarker <msarker86@gmail.com>
2021-04-13 16:04:36 +0000

    Commented out right now, but includes event listener to print out the number of forms in accordance to how many habits selected for habit set up

---
+++ static/habit_setup.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//const showHabitForms = () =>{
+    //let numberOfForms = Number($('#new_habits').html());
+    //show form numberOfForms times
+
+
+
+//}
+
+//$('#new_habits').on('submit', () =>{
+    //showHabitForms
+
+//})
