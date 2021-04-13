d2e14497b87edee4a29afede98d7084aa022d50a
Marjana Sarker <msarker86@gmail.com>
2021-04-13 20:42:06 +0000

    Changes made to import all classes from model.py

--- seed.py
+++ seed.py
@@ -1,8 +1,16 @@
+"""File to seed the habits database to run tests on functions"""
+from datetime import datetime
 import crud
-import model
-import server
+from model import db, User, User_habit, Habit, Habit_log, Journal_log, Messages, connect_to_db
 
-os.system('dropdb habits')
-os.system('createdb habits')
-model.connect_to_db(server.app)
-model.db.create_all()
+
+
+
+
+if __name__=='__main__':
+    from server import app
+    connect_to_db(app)
+    os.system('dropdb habits')
+    os.system('createdb habits')
+    model.connect_to_db(server.app)
+    model.db.create_all()
