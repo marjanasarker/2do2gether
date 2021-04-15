a12138eb2fbdfd36ed7703b0815477df3f359748
Marjana Sarker <msarker86@gmail.com>
2021-04-15 16:32:38 +0000

    Mock data to populate database

--- seed.py
+++ seed.py
@@ -1,12 +1,14 @@
 """File to seed the habits database to run tests on functions"""
 from datetime import datetime
 import crud
+import os
+import server
 from model import db, User, User_habit, Habit, Habit_log, Journal_log, Messages, connect_to_db
 
 
-user_1 = User(fname="Andrew" lname='Gerber', email='andrew.gerber@gmail.com', password='and123')
-user_2 = User(fname="Lana" lname='Landor', email='lana.landor@gmail.com', password='lan123')
-user_3 = User(fname="Wana" lname='Sour', email='wana.sour@gmail.com', password='wan123')
+user_1 = User(fname="Andrew", lname='Gerber', email='andrew.gerber@gmail.com', password='and123')
+user_2 = User(fname="Lana", lname='Landor', email='lana.landor@gmail.com', password='lan123')
+user_3 = User(fname="Wana", lname='Sour', email='wana.sour@gmail.com', password='wan123')
 
 habit_1 = Habit(name='yoga')
 habit_2 = Habit(name='running')
@@ -23,10 +25,10 @@ user_habit_2=User_habit(user_id=1, habit_id=3, accountability_partner_id=2, goal
 user_habit_3=User_habit(user_id=2, habit_id=1, accountability_partner_id=1, goal=20, completed=False, name='yoga', type_of_execution='hours', start_date='04-13-2021', end_date='05-14-2021')
 user_habit_4=User_habit(user_id=2, habit_id=2, accountability_partner_id=1, goal=10, completed=False, name='running', type_of_execution='hours', start_date='04-13-2021', end_date='05-14-2021')
 
-habit_log_1=Habit_log(user_habit_id=1,journal_id=1,date_of='04-13-2021',progress=1)
-habit_log_2=Habit_log(user_habit_id=2,journal_id=2,date_of='04-13-2021',progress=1)
-habit_log_3=Habit_log(user_habit_id=3,journal_id=3,date_of='04-13-2021',progress=1)
-habit_log_4=Habit_log(user_habit_id=4,journal_id=4,date_of='04-13-2021',progress=0.5)
+habit_log_1=Habit_log(user_habit_id=1,journal_id=1,date_of='04-13-2021',log_in_time=1, progress=5)
+habit_log_2=Habit_log(user_habit_id=2,journal_id=2,date_of='04-13-2021',log_in_time=1, progress=4)
+habit_log_3=Habit_log(user_habit_id=3,journal_id=3,date_of='04-13-2021',log_in_time=1, progress=5)
+habit_log_4=Habit_log(user_habit_id=4,journal_id=4,date_of='04-13-2021',log_in_time=0.5, progress=5)
 
 messages_1=Messages(user_habit_id=1, sender_id=2, receiver_id=1, message_date='04-13-2021', message='Fantastic job finishing the task at hand today')
 messages_2=Messages(user_habit_id=2, sender_id=2, receiver_id=1, message_date='04-13-2021', message='I bet meditating was great to keep the stress at bay')
@@ -36,7 +38,6 @@ messages_4=Messages(user_habit_id=3, sender_id=1, receiver_id=2, message_date='0
 
 if __name__=='__main__':
     from server import app
-    connect_to_db(app)
     os.system('dropdb habits')
     os.system('createdb habits')
     connect_to_db(server.app)
