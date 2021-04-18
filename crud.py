420030fe7f95acb4e0a6ed07266333689797e14b
Marjana Sarker <msarker86@gmail.com>
2021-04-18 01:53:12 +0000

    Added back habit_id to creat_user_habit function

--- crud.py
+++ crud.py
@@ -12,9 +12,9 @@ def create_user(fname, lname, email, password):
     
     return user
 
-def create_user_habit(user_id, goal, name, type_of_execution, start_date, end_date):
+def create_user_habit(user_id, habit_id, goal, name, type_of_execution, start_date, end_date):
     """Create and return a new habit set up"""
-    user_habit = User_habit(user_id=user_id, goal=goal, name=name, 
+    user_habit = User_habit(user_id=user_id, habit_id=habit_id, goal=goal, name=name, 
                            type_of_execution=type_of_execution, 
                            start_date=start_date, end_date=end_date)
 
@@ -23,17 +23,17 @@ def create_user_habit(user_id, goal, name, type_of_execution, start_date, end_da
 
     return user_habit
 
-def create_habit(habit_id, name):
+def create_habit(name):
     """Create and return the user's habits for the month"""
-    habit = Habit(habit_id=habit_id, name=name)
+    habit = Habit(name=name)
 
     db.session.add(habit)
     db.session.commit()
 
-def create_habit_log(user_habit_id, journal_id, date_of, log_in_time, progress):
+def create_habit_log(user_habit_id, journal_id, date_of, log_in_time):
     """Create and return a new log for habit completion"""
     habit_log = Habit_log(user_habit_id=user_habit_id, journal_id=journal_id, 
-                           date_of=date_of, log_in_time=log_in_time, progress=progress) #should I add progress?
+                           date_of=date_of, log_in_time=log_in_time) #should I add progress?
 
     db.session.add(habit_log)
     db.session.commit()
@@ -66,6 +66,19 @@ def get_user_by_email(email):
     """Returns user with email"""
 
     return User.query.filter(User.email == email).first()
+
+def get_habits():
+    """Returns all habits in database in a list"""
+
+    habits_in_db = db.session.query(Habit.name).all()
+    habits_in_list = [habit for (habit,) in habits_in_db]
+    return habits_in_list
+
+def get_habit_id(habit_name):
+    """Returns habit id by taking habit_name as argument"""
+
+    user_habit = Habit.query.filter(Habit.name == habit_name).first()
+    return user_habit.habit_id
 
 def get_habits_by_user(user_id):
     """Return habits by user"""
