d5006e35e0296795173f1da28101cd58af1611c5
Marjana Sarker <msarker86@gmail.com>
2021-04-19 20:34:12 +0000

    Test login commented out right now, returns an error when run

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from server import app
-from model import connect_to_db
+from model import connect_to_db, db
 from flask import session
 
 
@@ -17,6 +17,14 @@ class FlaskTests(TestCase):
 
         result = self.client.get("/")
         self.assertIn(b"Welcome!", result.data)
+    
+    # def test_login(self):
+    #     """Tests the login page"""
+
+    #     result = self.client.post('/login', 
+    #                                 data={"email":"andrew.gerber@gmail.com", "password": "and123"}, 
+    #                                 follow_redirects=True)
+    #     self.assertIn(b'<h1>Habit Page</h1>', result.data)                            
 
 if __name__ == "__main__":
     import unittest
