a8e1e56b9b9166b08d310b1f9ff53ac79bf692e3
Marjana Sarker <msarker86@gmail.com>
2021-04-23 16:26:36 +0000

    Made changes to seed.py to add all the variables to database

--- seed.py
+++ seed.py
@@ -4,7 +4,11 @@ import crud
 import os
 import server
 from model import db, User, User_habit, Habit, Habit_log, Journal_log, Messages, connect_to_db
-
+from server import app
+os.system('dropdb habits')
+os.system('createdb habits')
+connect_to_db(server.app)
+db.create_all()
 
 user_1 = User(fname="Andrew", lname='Gerber', email='andrew.gerber@gmail.com', password='and123')
 user_2 = User(fname="Lana", lname='Landor', email='lana.landor@gmail.com', password='lan123')
@@ -25,20 +29,21 @@ user_habit_2=User_habit(user_id=1, habit_id=3, accountability_partner_id=2, goal
 user_habit_3=User_habit(user_id=2, habit_id=1, accountability_partner_id=1, goal=20, completed=False, name='yoga', type_of_execution='hours', start_date='04-13-2021', end_date='05-14-2021')
 user_habit_4=User_habit(user_id=2, habit_id=2, accountability_partner_id=1, goal=10, completed=False, name='running', type_of_execution='hours', start_date='04-13-2021', end_date='05-14-2021')
 
-habit_log_1=Habit_log(user_habit_id=1,journal_id=1,date_of='04-13-2021',log_in_time=1, progress=5)
-habit_log_2=Habit_log(user_habit_id=2,journal_id=2,date_of='04-13-2021',log_in_time=1, progress=4)
-habit_log_3=Habit_log(user_habit_id=3,journal_id=3,date_of='04-13-2021',log_in_time=1, progress=5)
-habit_log_4=Habit_log(user_habit_id=4,journal_id=4,date_of='04-13-2021',log_in_time=0.5, progress=5)
+habit_log_1=Habit_log(user_habit_id=1,journal_id=1,date_of='04-13-2021',log_in_time=1)
+habit_log_2=Habit_log(user_habit_id=2,journal_id=2,date_of='04-13-2021',log_in_time=1)
+habit_log_3=Habit_log(user_habit_id=3,journal_id=3,date_of='04-13-2021',log_in_time=1)
+habit_log_4=Habit_log(user_habit_id=4,journal_id=4,date_of='04-13-2021',log_in_time=0.5)
 
 messages_1=Messages(user_habit_id=1, sender_id=2, receiver_id=1, message_date='04-13-2021', message='Fantastic job finishing the task at hand today')
 messages_2=Messages(user_habit_id=2, sender_id=2, receiver_id=1, message_date='04-13-2021', message='I bet meditating was great to keep the stress at bay')
 messages_3=Messages(user_habit_id=3, sender_id=1, receiver_id=2, message_date='04-13-2021', message='Just 20 mins of yoga will make a huge difference, you still have time')
-messages_4=Messages(user_habit_id=3, sender_id=1, receiver_id=2, message_date='04-13-2021', message='Fantastic job on a great run!')
+messages_4=Messages(user_habit_id=4, sender_id=1, receiver_id=2, message_date='04-13-2021', message='Fantastic job on a great run!')
 
 
-if __name__=='__main__':
-    from server import app
-    os.system('dropdb habits')
-    os.system('createdb habits')
-    connect_to_db(server.app)
-    db.create_all()
+db.session.add_all([user_1,user_2,user_3])
+db.session.add_all([habit_1,habit_2,habit_3,habit_4])
+db.session.add_all([journal_1,journal_2,journal_3,journal_4])
+db.session.add_all([user_habit_1,user_habit_2,user_habit_3,user_habit_4])
+db.session.add_all([habit_log_1,habit_log_2,habit_log_3,habit_log_4])
+db.session.add_all([messages_1,messages_2,messages_3,messages_4])
+db.session.commit()
