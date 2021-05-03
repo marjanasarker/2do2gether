5445f27ef06c2ee45191877fe60471139287725d
Marjana Sarker <msarker86@gmail.com>
2021-05-03 04:42:39 +0000

    Deleted a test

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from server import app
 from model import connect_to_db, db, example_data
 from flask import session
+import os
 
 
 class FlaskTests(TestCase):
@@ -19,8 +20,7 @@ class FlaskTests(TestCase):
         result = self.client.get("/")
         self.assertIn(b"Welcome!", result.data)
 
-    
-
+ 
 class FlaskTestsDatabase(TestCase):
 
     def setUp(self):
@@ -30,6 +30,8 @@ class FlaskTestsDatabase(TestCase):
         app.config['TESTING'] = True
 
         #Connected to test database
+        os.system('dropdb testdb')
+        os.system('createdb testdb')
         connect_to_db(app, "postgresql:///testdb")
 
         #Creating tables and adding sample data
@@ -52,11 +54,9 @@ class FlaskTestsDatabase(TestCase):
                                    follow_redirects=True)
         self.assertIn(b'<h1>Sign-In</h1>', result.data)
     
-    def test_habit_page(self):
-        """Tests the habit creation page"""
-
-        result = self.client.get('/habit')
-        self.assertIn(b'<h1>Habit Page</h1>', result.data)                            
+    
+    
+                           
 
 if __name__ == "__main__":
     import unittest
